0190338b70390624f24cc440b18d22589ce3ef9b
BiaMayumi <109619902+BiaMayumi@users.noreply.github.com>
2023-10-18 15:39:43 -0300

    Calculadora simples

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+# Calculadora Simples
+def soma(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mult(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    if n1 == 0 and n2 == 0:
+        print('Erro')
+    return n1 / n2
---
+++ main.py
@@ -0,0 +1,70 @@
+import calculadora
+
+# Implementação da calculadora simples
+
+while True:
+
+
+# Apresentação
+    print('\n\t\t\t -- Calculadora Simples --')
+
+    # Menu
+    print('1. Soma')
+    print('2. Subtração')
+    print('3. Multiplicação')
+    print('4. Divisão')
+    print('5. Sair')
+
+    # Ler a opção de escolha do usuário
+    op = int(input('\n\tOpção: '))
+
+    if op == 1:
+        print('\nSoma\n')
+         # Entrada
+        v1 = int(input('Informe o valor 1: '))
+        v2 = int(input('Informe o valor 2: '))
+
+        # Processamento
+        total = calculadora.soma(v1, v2)
+        print(f'A soma desses números será {total}')
+
+    elif op == 2:
+        print('\nSubtração\n')
+
+        # Entrada
+        v1 = int(input('Informe o valor 1: '))
+        v2 = int(input('Informe o valor 2: '))
+
+        total = calculadora.sub(v1, v2)
+        print(f'A subtração desses números será {total}')
+
+    elif op == 3:
+        print('\nMultiplicação\n')
+
+        # Entrada
+        v1 = int(input('Informe o valor 1: '))
+        v2 = int(input('Informe o valor 2: '))
+
+        total = calculadora.mult(v1, v2)
+        print(f'A Multiplicação desses números será {total}')
+
+    elif op == 4:
+        print('\nDivisão\n')
+
+
+        # Entrada
+        v1 = int(input('Informe o valor 1: '))
+        v2 = int(input('Informe o valor 2: '))
+
+        if v1 == 0 and v2 == 0:
+            print('Não é possível dividir por 0')
+
+        else:
+            total = calculadora.div(v1, v2)
+            print(f'A Divisão desses números será {total}')
+
+    elif op == 5:
+        print('Até mais')
+        break
+    else:
+        print(f'Opçao {op} incorreta\n')
